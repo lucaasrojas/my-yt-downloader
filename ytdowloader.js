237f3b698b86a7a00d40373a191ac61d830e9fa5
Lucas Rojas <lucas.rojas@live.com.ar>
2024-12-28 10:56:29 -0300

    add options for video and audio

--- ytdowloader.js
+++ ytdowloader.js
@@ -1,42 +1,82 @@
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-const { error } = require('console');
+const ytdl = require("ytdl-core");
+const fs = require("fs");
 
-// Opciones para la descarga
-const options = {
-  quality: 'highestaudio',
-  filter: 'audioonly'
+const args = process.argv;
+const getArgumentIndex = (argString) =>{
+  	return args.findIndex((arg) => arg === argString)
+}
+const getArgument = (argString) => {
+  const index = getArgumentIndex(argString)
+  const argument = args[index + 1]
+  const isArgument = !(argument).includes('-')
+  return index > -1 && isArgument ? argument : null;
+};
+
+const video = getArgumentIndex("-v") > -1;
+const audio = getArgumentIndex("-a") > -1;
+const filePath = getArgument("-f");
+
+const getVideo = async (title, link) => {
+  if (!fs.existsSync(`${title}.mp4`)) {
+    const videoStream = ytdl(link);
+    console.log("title", title);
+    const writeStream = fs.createWriteStream(`${title}.mp4`);
+    writeStream.on("error", (err) => {
+      console.log("ERR", err);
+    });
+    await videoStream.pipe(writeStream);
+  } else {
+    console.log(`File ${title}.mp4 already exists`);
+  }
+};
+
+const getAudio = async (title, link) => {
+  if (!fs.existsSync(`${title}.mp3`)) {
+    const videoStream = ytdl(link, {
+      quality: "highestaudio",
+      filter: "audioonly",
+    });
+    console.log("title", title);
+    const writeStream = fs.createWriteStream(`${title}.mp3`);
+    writeStream.on("error", (err) => {
+      console.log("ERR", err);
+    });
+    await videoStream.pipe(writeStream);
+  } else {
+    console.log(`File ${title}.mp3 already exists`);
+  }
 };
 
 // Descargar el video
 const downloadVideo = (link) => {
-  ytdl.getBasicInfo(link).then(async (basicInfo) => {
-    let title = basicInfo.videoDetails.title
-    if (!fs.existsSync(`${title}.mp3`)) {
+  ytdl
+    .getBasicInfo(link)
+    .then(async (basicInfo) => {
+      let title = basicInfo.videoDetails.title;
+      if (audio) {
+        await getAudio(title, link);
+      }
+      if (video) {
+        await getVideo(title, link);
+      }
+    })
+    .catch((err) => console.log("ERR - getBasicInfo", err));
+};
 
-      const videoStream = ytdl(link, options);
-      console.log("title", title)
-      const writeStream = fs.createWriteStream(`${title}.mp3`)
-      writeStream.on("error", (err) => {
-        console.log("ERR", err)
-      })
-      await videoStream.pipe(writeStream)
-    }
-  })
+// ===================================================
+
+
+
+if (!video && !audio) {
+  throw new Error("Please specify -v or -a");
 }
-
-const args = process.argv
-const getArgument = (argString) => {
-  return args[args.findIndex(arg => arg === argString) + 1]
-}
-
-const filePath = getArgument("-f")
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  const videoList = data.replace(/(\r\n|\n|\r)/gm, "").split(",")
-  console.log("file data", videoList)
+if(getArgumentIndex('-f') > -1 && !filePath){
+  throw Error('When using -f you have to specify path')
+} 
+fs.readFile(filePath, "utf8", (err, data) => {
+  const videoList = data.replace(/(\r\n|\n|\r)/gm, "").split(",");
+  console.log("file data", videoList);
   videoList.forEach(async (link) => {
-    await downloadVideo(link)
-  })
-})
-
+    await downloadVideo(link);
+  });
+});
